9ee5d2255bb1268e1c35db993f5bfe669b0c0948
CHAYMA DALLEL <chayma.dallel@enis.tn>
2026-03-24 23:46:20 +0100

    Add Supprimer servlet to handle item removal

---
+++ Supprimer.java
@@ -0,0 +1,54 @@
+package com.boutique;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Servlet implementation class Supprimer
+ */
+public class Supprimer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Supprimer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        HttpSession session = request.getSession();
+        ArrayList<Article> panier = (ArrayList<Article>) session.getAttribute("panier");
+
+        if (panier != null && id != null) {
+            for (int i = 0; i < panier.size(); i++) {
+                if (panier.get(i).getId().equals(id)) {
+                    panier.remove(i);
+                    break; 
+                }
+            }
+            session.setAttribute("panier", panier);
+        }
+        response.sendRedirect("afficherPanier.jsp");
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
